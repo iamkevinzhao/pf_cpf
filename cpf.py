85cf9aac11c586a175f76b65eed3e53ab69476b9
Kevin ZHAO <hecheng.zhao@outlook.com>
2019-10-27 22:06:24 +0900

    flag shaped demo v1

--- cpf.py
+++ cpf.py
@@ -6,74 +6,6 @@ from scipy.linalg import block_diag
 import warnings
 warnings.simplefilter('ignore', np.RankWarning)
 
-# x = -0.3
-# y = 0
-# phi = 0 * math.pi / 180
-# h = 1
-# k = 0
-# xx = x * math.cos(phi) - y * math.sin(phi) + h
-# yy = x * math.cos(phi) - y * math.sin(phi) + k
-# print(xx, yy)
-# exit()
-
-initial_measurement = []
-filename = "/home/kevin/pf_cpf/data/orb_pose.txt"
-stereo_ts = []
-stereo_pose = []
-i = -1
-with open(filename) as f:
-    for line in f:
-        parse = line.strip().split()
-        stereo_ts.append(int(parse[0]))
-        measurement = [float(parse[1]) * 1.06, float(parse[2]) * 1.06, float(parse[3])]
-        # if not len(initial_measurement):
-        #     initial_measurement = measurement.copy()
-        #     measurement = [0, 0, 0]
-        # else:
-        #     measurement[0] = measurement[0] - initial_measurement[0]
-        #     measurement[1] = measurement[1] - initial_measurement[1]
-        # print(measurement)
-        stereo_pose.append(measurement)
-
-plt.plot([stereo_pose[i][0] for i in range(len(stereo_pose))],
-         [stereo_pose[i][1] for i in range(len(stereo_pose))],
-         '-', label='stereo', markersize=1)
-
-filename = "/home/kevin/pf_cpf/data/pamr_pose.txt"
-lidar_ts = []
-lidar_pose = []
-with open(filename) as f:
-    for line in f:
-        parse = line.strip().split()
-        if len(lidar_ts):
-            if int(parse[0]) == lidar_ts[-1]:
-                continue
-        lidar_ts.append(int(parse[0]))
-        x = -0.3
-        y = 0
-        phi = float(parse[3])
-        h = float(parse[1])
-        k = float(parse[2])
-        xx = x * math.cos(phi) - y * math.sin(phi) + h
-        yy = x * math.sin(phi) + y * math.cos(phi) + k
-
-        if len(lidar_pose):
-            xx = xx - (lidar_pose[0][0] - 0)
-            yy = yy - (lidar_pose[0][1] - 0)
-        lidar_pose.append([xx, yy, float(parse[3])])
-        # lidar_pose.append([float(parse[1]), float(parse[2]), float(parse[3])])
-
-print(len(stereo_ts))
-
-plt.plot([lidar_pose[i][0] for i in range(len(lidar_pose))],
-         [lidar_pose[i][1] for i in range(len(lidar_pose))],
-         '-', label='lidar', markersize=1)
-
-plt.legend(loc='upper right')
-plt.gca().set_aspect('equal')
-plt.show()
-exit()
-
 class StateFusion:
 
     def __init__(self):
@@ -86,10 +18,10 @@ class StateFusion:
         # cov = np.diag([0.1, 0.1, 0.1])
         self.cov = np.diag([0.1, 0.1, 0.1])
         self.t_approx = 0.2 * 1e9
-        self.Q = np.diag([0.0221, 0.0221, 0.033]) * 0.03
+        self.Q = np.diag([0.0221, 0.0221, 0.033]) # * 0.3
         # Q = np.diag([0.007, 0.007, 0.0733])
-        self.lidar_cov = np.diag([0.00334, 0.00334, 0.0433]) * 0.03
-        self.stereo_cov = np.diag([0.0153, 0.0153, 0.0388]) * 0.03
+        self.lidar_cov = np.diag([10.0334, 10.0334, 10.0433]) # * 0.3
+        self.stereo_cov = np.diag([0.0353, 0.0353, 0.0388]) # * 0.3
         # self.stereo_cov = np.diag([0.0203, 0.0103, 0.0388]) * 0.03
         self.d9 = 16.9
         self.trace_t = []
@@ -213,46 +145,42 @@ state_fusion = StateFusion()
 outlier_removal = StateFusion()
 outlier_removal.d9 = 10000
 
-stop = 1570543968000000000
-filename = "/home/kevin/pf_cpf/data/pamr_pose.txt"
-lidar_ts = []
-lidar_pose = []
-with open(filename) as f:
-    for line in f:
-        parse = line.strip().split()
-        if int(parse[0]) > stop:
-            break
-        lidar_ts.append(int(parse[0]))
-        lidar_pose.append([float(parse[1]), float(parse[2]), float(parse[3])])
-
 filename = "/home/kevin/pf_cpf/data/orb_pose.txt"
 stereo_ts = []
 stereo_pose = []
+with open(filename) as f:
+    for line in f:
+        parse = line.strip().split()
+        stereo_ts.append(int(parse[0]))
+        measurement = [float(parse[1]) * 1.06, float(parse[2]) * 1.06, float(parse[3])]
+        stereo_pose.append(measurement)
+
+filename = "/home/kevin/pf_cpf/data/pamr_pose.txt"
+lidar_ts = []
+lidar_pose = []
 i = -1
 with open(filename) as f:
     for line in f:
         i = i + 1
-        if (i % 5) != 0:
+        if (i % 10) != 0:
             continue
         parse = line.strip().split()
-        if int(parse[0]) > stop:
-            break
-        stereo_ts.append(int(parse[0]))
-        measurement = [float(parse[1]), float(parse[2]), float(parse[3]) + math.pi]
-        state_fusion.wrap_angles(measurement)
-        if measurement[2] > (150 * math.pi / 180):
-            measurement[2] -= math.pi
-        stereo_pose.append(measurement)
+        if len(lidar_ts):
+            if int(parse[0]) == lidar_ts[-1]:
+                continue
+        lidar_ts.append(int(parse[0]))
+        x = -0.3
+        y = 0
+        phi = float(parse[3])
+        h = float(parse[1])
+        k = float(parse[2])
+        xx = x * math.cos(phi) - y * math.sin(phi) + h
+        yy = x * math.sin(phi) + y * math.cos(phi) + k
 
-for i in range(0, len(stereo_pose)):
-    stereo_pose[i][0] = -stereo_pose[i][0]
-    stereo_pose[i][1] = -stereo_pose[i][1]
-
-dx = lidar_pose[0][0] - stereo_pose[0][0]
-dy = lidar_pose[0][1] - stereo_pose[0][1]
-for i in range(0, len(stereo_pose)):
-    stereo_pose[i][0] = stereo_pose[i][0] + dx
-    stereo_pose[i][1] = stereo_pose[i][1] + dy
+        if len(lidar_pose):
+            xx = xx - (lidar_pose[0][0] - 0)
+            yy = yy - (lidar_pose[0][1] - 0)
+        lidar_pose.append([xx, yy, float(parse[3])])
 
 stereo_idx = 0
 lidar_idx = 0
@@ -269,9 +197,6 @@ while (stereo_idx < (len(stereo_ts) - 1)) and (lidar_idx < (len(lidar_ts) - 1)):
                    lidar_pose[lidar_idx][2]],
              'type': 'lidar'}
 
-    # if (stereo['t'] > (stop - 1000000000)) or (lidar['t'] > (stop - 1000000000)):
-    #     break
-    # print(stereo['t'], lidar['t'])
     if stereo['t'] < lidar['t']:
         state_fusion.sensor_cb(stereo.copy())
         outlier_removal.sensor_cb(stereo.copy())
@@ -283,23 +208,20 @@ while (stereo_idx < (len(stereo_ts) - 1)) and (lidar_idx < (len(lidar_ts) - 1)):
         if lidar_idx < (len(lidar_ts) - 1):
             lidar_idx = lidar_idx + 1
 
-for i in range(len(lidar_pose)):
-    if abs(lidar_pose[i][0]) > 10 or abs(lidar_pose[i][1]) > 10:
-        lidar_pose[i][0] = 0
-        lidar_pose[i][1] = 0
+# for i in range(len(lidar_pose)):
+#     if abs(lidar_pose[i][0]) > 10 or abs(lidar_pose[i][1]) > 10:
+#         lidar_pose[i][0] = 0
+#         lidar_pose[i][1] = 0
 
-# print('-------------')
-# print(state_fusion.trace[1], lidar_pose[1])
-# print('-------------')
 
-del lidar_pose[-3:]
-del lidar_ts[-3:]
-del stereo_pose[-2:]
-del stereo_ts[-2:]
-del state_fusion.trace[-2:]
-del state_fusion.trace_t[-2:]
-del outlier_removal.trace[-1:]
-del outlier_removal.trace_t[-1:]
+# del lidar_pose[-3:]
+# del lidar_ts[-3:]
+# del stereo_pose[-2:]
+# del stereo_ts[-2:]
+# del state_fusion.trace[-2:]
+# del state_fusion.trace_t[-2:]
+# del outlier_removal.trace[-1:]
+# del outlier_removal.trace_t[-1:]
 
 plt.figure(0)
 plt.plot([lidar_pose[i][0] for i in range(len(lidar_pose))],
@@ -315,20 +237,25 @@ plt.plot([outlier_removal.trace[i][0] for i in range(len(outlier_removal.trace))
          [outlier_removal.trace[i][1] for i in range(len(outlier_removal.trace))],
          '-', label='cpf_outlier', markersize=1)
 plt.title('global position / meter')
-ground = np.loadtxt('data/groundtruth.txt')
-print("(%f, %f, %f) vs. (%f, %f, %f) " %
-      (ground[-1, 0], ground[-1, 1], ground[-1, 2],
-      state_fusion.trace[-1][0], state_fusion.trace[-1][1], state_fusion.trace[-1][2]))
-plt.plot(ground[:, 0], ground[:, 1], '-', label='ground-truth', markersize=1)
-plt.legend(loc='lower right')
+# ground = np.loadtxt('data/groundtruth.txt')
+# print("(%f, %f, %f) vs. (%f, %f, %f) " %
+#       (ground[-1, 0], ground[-1, 1], ground[-1, 2],
+#       state_fusion.trace[-1][0], state_fusion.trace[-1][1], state_fusion.trace[-1][2]))
+# plt.plot(ground[:, 0], ground[:, 1], '-', label='ground-truth', markersize=1)
+plt.legend(loc='upper right')
 plt.gca().set_aspect('equal')
+
+# for i in range(0, len(state_fusion.d)):
+#     if state_fusion.d[i][0] < 0.1:
+#         print(i)
 
 plt.figure(1)
 plt.plot([state_fusion.d[i][0] for i in range(0, len(state_fusion.d))],
          [state_fusion.d[i][1] for i in range(0, len(state_fusion.d))],
          '-', label='d', markersize=1)
-plt.plot([state_fusion.d[i][0] for i in range(0, len(state_fusion.d))],
-         [16.9 for i in range(0, len(state_fusion.d))], '-')
+# plt.plot([state_fusion.d[i][0] for i in range(0, len(state_fusion.d))],
+#          [16.9 for i in range(0, len(state_fusion.d))], '-')
+plt.legend(loc='upper right')
 plt.title('CPF distance over time')
 
 plt.figure(2)
@@ -344,10 +271,10 @@ plt.plot([state_fusion.trace_t[i] for i in range(len(state_fusion.trace_t))],
 plt.plot([outlier_removal.trace_t[i] for i in range(len(outlier_removal.trace_t))],
          [math.degrees(outlier_removal.trace[i][2]) for i in range(len(outlier_removal.trace))],
          '-', label='cpf_outlier', markersize=1)
-ground = np.delete(ground, -1, 0)
-plt.plot([ground[i][4] for i in range(len(ground))],
-         [math.degrees(ground[i][2]) for i in range(len(ground))],
-         '-', label='groundtruth', markersize=1)
+# ground = np.delete(ground, -1, 0)
+# plt.plot([ground[i][4] for i in range(len(ground))],
+#          [math.degrees(ground[i][2]) for i in range(len(ground))],
+#          '-', label='groundtruth', markersize=1)
 plt.legend(loc='upper right')
 plt.title('global heading (degree) over time')
 
